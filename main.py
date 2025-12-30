e5783a172301e634a5c11b6cd55015242d3a3cf3
MohitMunjal54 <30656245+MohitMunjal54@users.noreply.github.com>
2025-12-30 11:20:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+#from telegram import Bot
+#from scheduler import start_scheduler
+#from config import TELEGRAM_BOT_TOKEN
+
+#bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+#if __name__ == "__main__":
+   #start_scheduler(bot)
 from telegram import Bot
-from scheduler import start_scheduler
-from config import TELEGRAM_BOT_TOKEN
+from config import TELEGRAM_BOT_TOKEN, TELEGRAM_CHAT_ID
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 if __name__ == "__main__":
-    start_scheduler(bot)
+    bot.send_message(
+        chat_id=TELEGRAM_CHAT_ID,
+        text="🚀 Bot is LIVE. Railway deployment successful."
+    )
